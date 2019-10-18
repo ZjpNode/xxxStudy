3dd2fff33a2e063c25ee23005ca30b112d1fbbe0
ZjpNode <461584040@qq.com>
2019-10-18 11:14:53 +0800

    为天气预报接口添加appid和appsecret字段

--- demo/weather_forecast/js/app.js
+++ demo/weather_forecast/js/app.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: jiapeng.Zheng
+ * @Date: 2019-05-14 10:24:39
+ * @LastEditors: jiapeng.Zheng
+ * @LastEditTime: 2019-10-18 11:13:33
+ * @Description:
+ */
+const SECRE = '&appid=17324435&appsecret=N3PEnuVX'
+
 let main = new Vue({
-  el: "#main",
+  el: '#main',
   data: {
-    nowTime: dayjs().format("HH:mm:ss"),
+    nowTime: dayjs().format('HH:mm:ss'),
     weatherData: {
       data: []
     }
@@ -10,47 +19,51 @@ let main = new Vue({
   computed: {},
   created() {
     setInterval(() => {
-      this.nowTime = dayjs().format("HH:mm:ss");
-    }, 1000);
+      this.nowTime = dayjs().format('HH:mm:ss')
+    }, 1000)
     setInterval(() => {
-      this.getData();
-    }, 60 * 60 * 1000);
-    this.getData();
+      this.getData()
+    }, 60 * 60 * 1000)
+    this.getData()
   },
   methods: {
     getPinyin(word) {
       // console.log(window.pinyinUtil);
-      return pinyinUtil.getPinyin(word, "", false, false);
+      return pinyinUtil.getPinyin(word, '', false, false)
     },
     getData() {
       axios
-        .get("https://www.tianqiapi.com/api/?version=v1")
+        .get('https://www.tianqiapi.com/api/?version=v1' + SECRE)
         .then(response => {
-          if (response.status === 200) {
+          if (response.status === 200 && !response.data.errcode) {
             this.weatherData = Object.assign(
               {},
               this.weatherData,
               response.data
-            );
+            )
+          } else {
+            console.error(response)
           }
         })
         .catch(error => {
-          alert(error);
-        });
+          alert(error)
+        })
       axios
-        .get("https://www.tianqiapi.com/api/?version=v6")
+        .get('https://www.tianqiapi.com/api/?version=v6' + SECRE)
         .then(response => {
-          if (response.status === 200) {
+          if (response.status === 200 && !response.data.errcode) {
             this.weatherData = Object.assign(
               {},
               this.weatherData,
               response.data
-            );
+            )
+          } else {
+            console.error(response)
           }
         })
         .catch(error => {
-          alert(error);
-        });
+          alert(error)
+        })
     }
   }
-});
+})
