1fc35e646dbc981caa7f48efc1c747a4c489587d
ZjpNode <461584040@qq.com>
2019-11-14 16:35:13 +0800

    优化选择排序

--- Algorithm/src/sort/selectSort.js
+++ Algorithm/src/sort/selectSort.js
@@ -2,7 +2,7 @@
  * @Author: jiapeng.Zheng
  * @Date: 2019-11-11 11:26:31
  * @LastEditors: jiapeng.Zheng
- * @LastEditTime: 2019-11-13 10:20:01
+ * @LastEditTime: 2019-11-14 15:32:56
  * @Description: 选择排序,时间复杂度：O(N2), 稳定性：不稳定
  */
 let swap = require('../../lib/swap')
@@ -39,19 +39,29 @@ let selectSortV2 = arr => {
   let len = arr.length
   for (let i = 0; i < len / 2; i++) {
     let minIndex = i
-    let maxIndex = len - i - 1
+    let maxIndex = i // len - i - 1
     for (let j = i + 1; j <= len - i - 1; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+        continue
+      }
       if (arr[j] > arr[maxIndex]) {
         maxIndex = j
       }
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j
-      }
       _time++
     }
-
-    swap(arr, i, minIndex)
-    swap(arr, len - i - 1, maxIndex)
+    if (i !== minIndex) {
+      swap(arr, i, minIndex)
+      // 原来的第一个元素已经与下标为minPoint的元素交换了位置
+      // 如果之前maxPoint指向的是第一个元素，那么需要将maxPoint重新指向array[minPoint]
+      // 因为现在array[minPoint]存放的才是之前第一个元素中的数据
+      if (maxIndex === i) {
+        maxIndex = minIndex
+      }
+    }
+    if (len - i - 1 !== maxIndex) {
+      swap(arr, len - i - 1, maxIndex)
+    }
   }
   console.log('time: ', _time)
   return arr
