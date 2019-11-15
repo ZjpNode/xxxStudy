2cbf35c4056317552d2e4653fd518a7b2fe11e92
ZjpNode <461584040@qq.com>
2019-11-15 17:39:48 +0800

    update

--- Algorithm/src/sort/index.js
+++ Algorithm/src/sort/index.js
@@ -5,7 +5,7 @@
  * @LastEditTime: 2019-11-14 17:39:15
  * @Description:
  * 时间复杂度：描述该算法在处理大量数据时，总的来说其时间效率的参考；
- * 稳定性：描述算法对原始序列处理前后，该序列相等大小的元素前后位置是否发生改变
+ * 稳定性：描述算法对原始序列处理前后，该序列相等大小的元素前后位置是否发生改变。
  */
 exports.bubbleSort = require('./bubbleSort')
 exports.selectSort = require('./selectSort')
